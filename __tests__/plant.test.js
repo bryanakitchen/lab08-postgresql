a6092e5f77a8b0a648f7e7e0af1d5db6a093a9d4
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-10 15:41:09 -0500

    set up tables and tests and constructors of classes

---
+++ __tests__/plant.test.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
+const app = require('../lib/app');
+const Plant = require('../lib/models/Plant');
+
+describe('CRUD routes for Plant model', () => {
+  it('', () => {
+
+  });
+});
+
---
+++ __tests__/snack.test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
+const app = require('../lib/app');
+const Snack = require('../lib/models/Snack');
+
+describe('CRUD routes for Snack model', () => {
+  it('', () => {
+
+  });
+});
